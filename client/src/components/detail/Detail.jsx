98ad5aab9ef519c94f3bec62d6abfc75c17b88ee
FBonino <boninoffranco@gmail.com>
2023-03-12 12:28:53 -0300

    Add button to enable/disable editing ToDo in detail modal

--- client/src/components/detail/Detail.jsx
+++ client/src/components/detail/Detail.jsx
@@ -1,47 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Detail.module.css";
 
 const Detail = ({ detail, setDetail, showDetail, handleClose, updateToDo }) => {
+  const [isEditing, setIsEditing] = useState(false)
 
   const handleChange = ({ target }) => setDetail({ ...detail, [target.name]: target.value })
 
   const handleSubmit = async e => {
     e.preventDefault()
     await updateToDo(detail)
+    closeModal()
+  }
+
+  const closeModal = () => {
     setDetail({ id: "", title: "", description: "", priority: 1, state: 1 })
+    setIsEditing(false)
     handleClose()
   }
 
   return showDetail && (
     <>
-      <div className={style.background} onClick={handleClose} />
-      <form onSubmit={handleSubmit} className={style.container}>
-        <div className={style.subcontainer}>
-          <label htmlFor="title"> Title </label>
-          <input name="title" onChange={handleChange} value={detail.title} />
+      <div className={style.background} onClick={closeModal} />
+      <div className={style.container}>
+        <div className={style.buttons}>
+          <button className={style.button} onClick={() => isEditing ? closeModal() : setIsEditing(true)}> {isEditing ? "Cancel" : "Edit"} </button>
+          {
+            isEditing && <button className={style.button} onClick={handleSubmit}> Update! </button>
+          }
         </div>
-        <div className={style.subcontainer}>
-          <label htmlFor="description"> Description </label>
-          <textarea name="description" onChange={handleChange} value={detail.description} rows="10" cols="45" />
-        </div>
-        <div className={style.subcontainer}>
-          <label htmlFor="priority"> Priority </label>
-          <select name="priority" onChange={handleChange} value={detail.priority}>
-            {
-              [...Array(10)].map((_, i) => <option key={i} value={i + 1}> {i + 1} </option>)
-            }
-          </select>
-        </div>
-        <div className={style.subcontainer}>
-          <label htmlFor="state"> State </label>
-          <select name="state" onChange={handleChange} value={detail.state}>
-            <option value={1}> New </option>
-            <option value={2}> Doing </option>
-            <option value={3}> Done </option>
-          </select>
-        </div>
-        <input type="submit" value="Update!" />
-      </form>
+        <form className={style.form} autoComplete="off">
+          <div className={style.subcontainer}>
+            <label htmlFor="title"> Title </label>
+            <input className={style.input} name="title" onChange={handleChange} value={detail.title} readOnly={!isEditing} />
+          </div>
+          <div className={style.subcontainer}>
+            <label htmlFor="description"> Description </label>
+            <textarea className={style.input} name="description" onChange={handleChange} value={detail.description} rows="10" cols="45" readOnly={!isEditing} />
+          </div>
+          <div className={style.subcontainer}>
+            <label htmlFor="priority"> Priority </label>
+            <select className={style.select} name="priority" onChange={handleChange} value={detail.priority} disabled={!isEditing}>
+              {
+                [...Array(10)].map((_, i) => <option key={i} value={i + 1}> {i + 1} </option>)
+              }
+            </select>
+          </div>
+          <div className={style.subcontainer}>
+            <label htmlFor="state"> State </label>
+            <select className={style.select} name="state" onChange={handleChange} value={detail.state} disabled={!isEditing}>
+              <option value={1}> New </option>
+              <option value={2}> Doing </option>
+              <option value={3}> Done </option>
+            </select>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
